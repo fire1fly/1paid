54e4a20b56255fe367c9af83e3148cf96a0462bc
fire1fly <danyseidev@gmail.com>
2023-08-30 07:03:30 +0800

    feat: add outside click of menu

--- src/js/components/mobileSidebar.ts
+++ src/js/components/mobileSidebar.ts
@@ -3,7 +3,19 @@ export function mobileSidebar() {
   const toggleBtn = document.querySelector('.menu-open');
 
   toggleBtn?.addEventListener("click", () => {
-    toggleBtn.classList.toggle("_to-open");
+    toggleBtn?.classList.toggle("_to-open");
     sidebarEl?.classList.toggle("_hide");
   });
+
+  if (document.documentElement.clientWidth <= 1440) {
+    document.addEventListener("click", (e) => {
+      const target = e.target as unknown as HTMLElement
+      const sidebar = target.closest(".menu");
+      const btn = target.closest(".menu-open");
+      if (!sidebar && !btn) {
+        toggleBtn?.classList.remove("_to-open");
+        sidebarEl?.classList.remove("_hide");
+      }
+    })
+  }
 }
